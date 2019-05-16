a35be1a6d044c9524d5d86fc61f301b37053f388
Dongliang Mu <mudongliangabcd@gmail.com>
2019-05-15 17:42:46 -0700

    add proc_entry example

---
+++ TestProcEntry/README.md
@@ -0,0 +1,31 @@
+# TestProcEntry 
+
+Test `proc_create` and `proc_remove` in the Linux Kernel 4.9. Note that API related Proc system changes from 3.10, see detail in the reference.
+
+## Module Compilation
+
+```sh
+sudo make
+```
+
+## Module Cleanup
+
+```sh
+sudo make clean
+```
+
+## Module Installation
+
+```sh
+sudo make install
+```
+
+## Module Uninstallation
+
+```sh
+sudo make uninstall
+```
+
+## Reference
+
+[implicit declaration of function 'create_proc_entry'](https://stackoverflow.com/questions/26808325/implicit-declaration-of-function-create-proc-entry)
---
+++ TestProcEntry/TestProcEntry.c
@@ -0,0 +1,48 @@
+#include <linux/module.h>
+#include <linux/init.h>
+#include <linux/moduleparam.h>
+#include <linux/kernel.h>   
+#include <linux/proc_fs.h>
+#include <asm/uaccess.h>
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("mudongliangabcd@gmail.com");
+MODULE_DESCRIPTION("HelloWorld Module Template");
+MODULE_VERSION("0.1");
+
+#define BUFSIZE  100
+ 
+static struct proc_dir_entry *ent;
+ 
+static ssize_t mywrite(struct file *file, const char __user *ubuf,size_t count, loff_t *ppos) 
+{
+	printk( KERN_DEBUG "write handler\n");
+	return -1;
+}
+ 
+static ssize_t myread(struct file *file, char __user *ubuf,size_t count, loff_t *ppos) 
+{
+	printk( KERN_DEBUG "read handler\n");
+	return 0;
+}
+ 
+static struct file_operations myops = 
+{
+	.owner = THIS_MODULE,
+	.read = myread,
+	.write = mywrite,
+};
+ 
+static int simple_init(void)
+{
+	ent = proc_create("mydev", 0660, NULL, &myops);
+	return 0;
+}
+ 
+static void simple_cleanup(void)
+{
+	proc_remove(ent);
+}
+ 
+module_init(simple_init);
+module_exit(simple_cleanup);
